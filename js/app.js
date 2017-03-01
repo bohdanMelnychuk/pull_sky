428ff2a39f5075ce6f8a3ce8e6640086be05d88c
User <User>
2017-03-01 13:49:40 +0200

    fix css issue

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+
 $(function(){
     $('.modal-cell').modal();
     $('#carousel').carousel({
@@ -12,5 +14,28 @@ $(function(){
     $('#carousel-client').carousel({
         interval : false
     })
+
+   
+//add active classes
+    $('.main-nav li a').click(function(){
+        $('.main-nav li.active-link').removeClass("active-link");
+        $(this).parent().addClass("active-link");
+    })
+    
+    $('.lang .languages li').click(function(){
+        $('.lang .languages li.active-lang').removeClass("active-lang");
+        $(this).addClass("active-lang");
+    })
+    
+    $('.news .nav-news li a').click(function(){
+        $('.news .nav-news li a.active-link-news').removeClass("active-link-news");
+        $(this).addClass("active-link-news");
+    })
+    
+    $('input[name=phone]').click(function(){
+        $(this).val("+38")
+    })
+    
+    //validation input fields
     
 })
